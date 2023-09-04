fff93036894a1d76193555adf13fad7c428032a3
Chandra Prakash <scp.thedevil@gmail.com>
2023-09-04 22:42:54 +0530

    Initial Structure and Tailwind Config needed for the project
    Signed-off-by: Chandra Prakash <scp.thedevil@gmail.com>

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import PopularProducts from "./PopularProducts";
+import CustomerReviews from "./CustomerReviews";
+import SuperQuality from "./SuperQuality";
+ 
+
+export { 
+    Hero, 
+    Footer, 
+    Services, 
+    SpecialOffer, 
+    Subscribe, 
+    PopularProducts, 
+    CustomerReviews, 
+    SuperQuality };
