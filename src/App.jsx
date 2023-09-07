242df524a5999961b29a3c879eedc9ce9e10fedd
Chandra Prakash <scp.thedevil@gmail.com>
2023-09-07 17:42:43 +0530

    Added Popular Products and dynamic rating
    Signed-off-by: Chandra Prakash <scp.thedevil@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -9,32 +9,53 @@ import {
   Subscribe,
   SuperQuality,
 } from "./sections";
+/**
+ * Renders the main application component
+ * @returns {JSX.Element} The main application component
+ */
 const App = () => {
   return (
     <main className='relative'>
+      {/* Render the navigation component */}
       <Nav />
+
       <section className='xl:padding-l wide:padding-r padding-b'>
+        {/* Render the hero component */}
         <Hero />
       </section>
+
       <section className='padding'>
+        {/* Render the popular products component */}
         <PopularProducts />
       </section>
+
       <section className='padding'>
+        {/* Render the super quality component */}
         <SuperQuality />
       </section>
+
       <section className='padding-x py-10'>
+        {/* Render the services component */}
         <Services />
       </section>
+
       <section className='padding'>
+        {/* Render the special offer component */}
         <SpecialOffer />
       </section>
+
       <section className='bg-pale-blue padding'>
+        {/* Render the customer reviews component */}
         <CustomerReviews />
       </section>
+
       <section className='padding-x sm:py-32 py-16 w-full'>
+        {/* Render the subscribe component */}
         <Subscribe />
       </section>
+
       <section className=' bg-black padding-x padding-t pb-8'>
+        {/* Render the footer component */}
         <Footer />
       </section>
     </main>
