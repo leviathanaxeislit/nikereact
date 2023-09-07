d2fdf3e8bfed5c52b78a289630bfe5f1164923a2
Chandra Prakash <scp.thedevil@gmail.com>
2023-09-07 15:25:29 +0530

    reworked ShoeCard and Added Documentation
    Signed-off-by: Chandra Prakash <scp.thedevil@gmail.com>

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,30 +1,51 @@
+/**
+ * Function to render a shoe card component
+ * 
+ * @param {Object} props - The properties passed to the component
+ * @param {Object} props.imgURL - The image URLs for the shoe
+ * @param {Function} props.changeBigShoeImage - The function to change the big shoe image
+ * @param {string} props.bigShoeImg - The URL of the currently displayed big shoe image
+ * @returns {React.Node} - The rendered shoe card component
+ */
 const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
-    const handleClick = () => {
-      if (bigShoeImg !== imgURL.bigShoe) {
-        changeBigShoeImage(imgURL.bigShoe);
-      }
-    };
-  
-    return (
-      <div
-        className={`border-2 rounded-xl ${
-          bigShoeImg === imgURL.bigShoe
-            ? "border-coral-red"
-            : "border-transparent"
-        } cursor-pointer max-sm:flex-1`}
-        onClick={handleClick}
-      >
-        <div className='flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
-          <img
-            src={imgURL.thumbnail}
-            alt='shoe colletion'
-            width={127}
-            height={103.34}
-            className='object-contain'
-          />
-        </div>
-      </div>
-    );
+  /**
+   * Event handler for when the shoe card is clicked
+   */
+  const handleClick = () => {
+    console.log('Clicked on shoe card');
+    if (bigShoeImg !== imgURL.bigShoe) {
+      changeBigShoeImage(imgURL.bigShoe);
+    }
   };
+
+  /**
+   * Determines the border class for the shoe card based on the currently displayed big shoe image
+   * 
+   * @type {string}
+   */
+  const borderClass = bigShoeImg === imgURL.bigShoe ? "border-coral-red" : "border-transparent";
+  
+  /**
+   * The class for the shoe card component
+   * 
+   * @type {string}
+   */
+  const cardClass = `border-2 rounded-xl ${borderClass} cursor-pointer max-sm:flex-1`;
+
+  console.log('Rendering shoe card');
+  return (
+    <div className={cardClass} onClick={handleClick}>
+      <div className='flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
+        <img
+          src={imgURL.thumbnail}
+          alt='shoe collection'
+          width={127}
+          height={103.34}
+          className='object-contain'
+        />
+      </div>
+    </div>
+  );
+};
   
   export default ShoeCard;
