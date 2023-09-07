a7f70532500f8ae2eb8372a01e6381acf4a2fad7
Chandra Prakash <scp.thedevil@gmail.com>
2023-09-08 00:16:02 +0530

    Completed About Section and Added framer motion effects
    Signed-off-by: Chandra Prakash <scp.thedevil@gmail.com>

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,30 +1,62 @@
+import React, { useEffect, useState } from "react";
 import Button from "../components/Button";
+import { shoe8 } from "../assets/images";
+import { motion, useAnimation } from "framer-motion";
 
 const SuperQuality = () => {
-    return (
-        <section
-        id="about-us"
-        className="flex justify-between items-center 
-        max-lg:flex-col gap-10 w-full max-container"
-        >
-<div className="flex flex-1 flex-col">
+  const [hovered, setHovered] = useState(false);
+  const controls = useAnimation();
 
+  useEffect(() => {
+    controls.start({
+      scale: hovered ? 1.1 : 1, // Increase scale when hovered
+      transition: { duration: 0.3 },
+    });
+  }, [hovered, controls]);
 
-        <h2 className="mt-10 font-palanquin text-4xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
-            The New Arrival
-          </span>
-          <br />
-          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl font-bold capitalize lg:max-w-lg">
+          We Provide You
+          <span className="text-coral-red"> Super</span>
+          <span className="text-coral-red "> Quality </span> Shoes
         </h2>
-        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
-          Discover stylish Nike arrivals, quality comfort, and innovation for
-          your active life.
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
         </p>
-    <Button label="View Details" />
-</div>
-        </section>
-    );
-}
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className="mt-11">
+          <Button label="View Details" />
+        </div>
+      </div>
+      <div
+        className="flex flex-1 justify-center items-center"
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      >
+        <motion.div
+          initial={{ opacity: 1, scale: 0.5 }}
+          animate={controls}
+        >
+          <img
+            src={shoe8}
+            alt="super quality shoe"
+            width={570}
+            height={522}
+            className="object-contain"
+          />
+        </motion.div>
+      </div>
+    </section>
+  );
+};
 
-export default SuperQuality
+export default SuperQuality;
