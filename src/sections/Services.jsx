e37b134888b7863bb41a4533d79d09ced1bea282
Chandra Prakash <scp.thedevil@gmail.com>
2023-09-08 20:57:58 +0530

    Added Hover effect using Framer Motion for service card
    Signed-off-by: Chandra Prakash <scp.thedevil@gmail.com>

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,14 +1,20 @@
 import { services } from "../constants";
 import  ServiceCard  from "../components/ServiceCard";
+import { motion } from 'framer-motion';
 
 const Services = () => {
     return (
-        <section className="max-container flex justify-center flex-wrap gap-9">
+      <section className="max-container flex justify-center flex-wrap gap-9">
         {services.map((service) => (
-            <ServiceCard key={service.name} {...service} />
+          <motion.div
+            key={service.name}
+            whileHover={{ scale: 1.05, boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)' }}
+          >
+            <ServiceCard {...service} />
+          </motion.div>
         ))}
-        </section>
-    );  
-}
-
-export default Services;
+      </section>
+    );
+  }
+  
+  export default Services;
